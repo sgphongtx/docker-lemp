c3f6c78c0a94f973b4ca011a99841ab4ddc9d3eb
PhongTX <phong.tran@webtretho.com>
2022-10-04 16:52:42 +0700

    Update docker

--- nhago3h.vn/public/index.php
+++ nhago3h.vn/public/index.php
@@ -20,7 +20,7 @@
     <body>
         <img src="https://tech.osteel.me/images/2020/03/04/hello.gif" alt="Hello there" class="center">
         <?php
-        $connection = new PDO('mysql:host=mysql;dbname=db_nhathepsaigon;charset=utf8', 'root', '04Bk1423!@#');
+        $connection = new PDO('mysql:host=mysql;dbname=db_nhathepsaigon;charset=utf8', 'root', '123456');
         $query      = $connection->query("SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_SCHEMA = 'db_nhathepsaigon'");
         $tables     = $query->fetchAll(PDO::FETCH_COLUMN);
 
